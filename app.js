1fe40ad88d257331417dceb5d3a5c494ad9a9e88
rameem2003 <rameem2019@gmail.com>
2023-03-13 08:55:14 +0600

    update

--- app.js
+++ app.js
@@ -12,12 +12,14 @@ const generate = document.getElementById("generate");
 const direction = document.getElementById("direction");
 
 const color_input = document.querySelectorAll(".color_input");
+let setdirection = "top";
+let setDegree = ""
 
 // display the color on input
 color_input.forEach(color => {
     color.addEventListener("input", () => {
         changeColor();
-        displayColor();
+        displayColor(setdirection);
     })
 })
 
@@ -34,28 +36,50 @@ function changeColor () {
 
 }
 
+
+
 // display color in window
-function displayColor () {
+function displayColor (direction) {
     const {leftColor, rightColor} = changeColor();
-    const colorCode = `linear-gradient(${leftColor}, ${rightColor})`
+    const colorCode = `linear-gradient(to ${direction}, ${leftColor}, ${rightColor})`
     displayColorWindow.style.backgroundImage = colorCode;
     console.log(colorCode);
     return colorCode;
 }
 
+// change gradient by direction
+direction.addEventListener("input", () => {
+    setdirection = direction.value;
+    console.log(setdirection);
+    switch (setdirection) {
+        case "left":
+            displayColor("left");
+            break;
+        
+        case "right":
+            displayColor("right");
+            break;
+
+        case "top":
+            displayColor("top");
+            break;
+
+        case "bottom":
+            displayColor("bottom");
+            break;
+        default:
+            break;
+    }
+})
+
 // generate color css code
 generate.addEventListener("click", () => {
-    let cssCode = displayColor();
+    let cssCode = displayColor(setdirection);
     outputWindow.innerHTML = `background-image: ${cssCode};`;
     let copyCode = `background-image: ${cssCode};`
     navigator.clipboard.writeText(copyCode);
 })
 
-// change gradient direction
-direction.addEventListener("input", () => {
-    console.log(direction.value);
-})
 
-
-changeColor()
-displayColor()
+changeColor();
+displayColor(setdirection);
